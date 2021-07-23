4ece51ba0e69f5e656f382a8f0013083e3aef18f
Guilherme Cabral <guilherme.mcabral@gmail.com>
2021-07-23 04:46:35 -0300

    O usuário agora pode registrar o seu nickname
    Para isso, foram adicionados um EditText, um TextView e um Button
    ao layout

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.doneButton.setOnClickListener { addNickName(it) }
+    }
+
+    private fun addNickName(view: View) {
+        val nicknameEditText = binding.nicknameEdit
+        val nicknameTextView = binding.nicknameText
+
+        nicknameTextView.text = nicknameEditText.text
+        nicknameEditText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
